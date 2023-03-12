384c125704ed294e7dd4eac46c76f631cffb95af
MaahiVohra <vohramaahi@gmail.com>
2023-03-12 17:43:12 +0530

    fix: meta tags

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -77,19 +77,19 @@ const MyApp: AppType = ({ Component, pageProps }) => {
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/apple-touch-icon.png"
+            href="apple-touch-icon.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="/favicon-32x32.png"
+            href="favicon-32x32.png"
           />
           <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="/favicon-16x16.png"
+            href="favicon-16x16.png"
           />
           <link rel="manifest" href="/site.webmanifest" />
           <meta name="title" content="Note - A notes taking app" />
@@ -121,7 +121,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             content="Organize your thoughts and ideas with ease using our intuitive notes taking app."
           />
           <meta property="twitter:image" content="" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="favicon.ico" />
         </Head>
         <Component
           {...pageProps}

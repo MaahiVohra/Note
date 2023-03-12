5ed540c831599d8434f0584ba623469c657cc913
MaahiVohra <vohramaahi@gmail.com>
2023-03-12 18:12:07 +0530

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,19 +42,19 @@ const Home: NextPage<NoteListProps> = ({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="title" content="Note - A notes taking app" />
@@ -75,7 +75,7 @@ const Home: NextPage<NoteListProps> = ({
         />
         <meta property="og:image" content="image-main.jpg" />
 
-        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:card" content="image-main.jpg" />
         <meta
           property="twitter:url"
           content="https://note-gamma-azure.vercel.app/"
